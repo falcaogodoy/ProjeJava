e96a4b30bc0dee30370a7f173a6c61728959d15f
falcaogodoy <falcaogodoy@gmail.com>
2022-07-18 16:50:40 -0300

    useState

--- ProjetoSpringReact/dsmeta/frontend/src/App.tsx
+++ ProjetoSpringReact/dsmeta/frontend/src/App.tsx
@@ -1,9 +1,23 @@
 
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
+
+
+
 function App() {
-  return (
-    
-      <h1>Ola Mundo</h1>
-          )
+  return(
+    <>
+    <Header/>
+    <main>
+        <section id="vendas">
+            <div className="principal-container">
+              <SalesCard/>
+            </div>
+        </section>
+    </main>       
+    </>
+  ) 
 }
 
 export default App
