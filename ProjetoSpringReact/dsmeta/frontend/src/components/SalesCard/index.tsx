e4fd9256809d2b2960db3b42b1e29e88a74c0f09
falcaogodoy <falcaogodoy@gmail.com>
2022-07-19 15:01:40 -0300

    arrumando front

--- ProjetoSpringReact/dsmeta/frontend/src/components/SalesCard/index.tsx
+++ ProjetoSpringReact/dsmeta/frontend/src/components/SalesCard/index.tsx
@@ -8,20 +8,17 @@ import { BASE_URL } from "../../utils/request";
 import { Sale } from "../../models/sale";
 
 function SalesCard() {
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
+  const [minDate, setMinDate] = useState(min);
+  const [maxDate, setMaxDate] = useState(new Date());
 
-    const min = new Date(new Date().setDate(new Date().getDate() - 365));
-    const [minDate, setMinDate] = useState(min);
-    const [maxDate, setMaxDate] = useState(new Date());
+  const [sales, setSales] = useState<Sale[]>([]);
 
-
-    const [sales, setSales] = useState<Sale[]>([])
-
-    useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
-        .then(response => {
-            setSales(response.data.content);
-        })
-    }, [])
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales`).then((response) => {
+      setSales(response.data.content);
+    });
+  }, []);
 
   return (
     <div className="principal-card">
@@ -58,52 +55,23 @@ function SalesCard() {
             </tr>
           </thead>
           <tbody>
-            {
-                sales.map(sale => {
-                   return (
-                    
-                   ) 
-                })
-            }
-            <tr>
-              <td className="show-responsi992"> #01</td>
-              <td className="data-show-responsi"> 08.08.08</td>
-              <td>Nome01</td>
-              <td className="show-responsi992"> 12</td>
-              <td className="show-responsi992"> 02</td>
-              <td> VALOR $ 01 </td>
-              <td>
-                <div className="btn-red-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="show-responsi992"> #02</td>
-              <td className="data-show-responsi"> 08.08.08</td>
-              <td>Nome02</td>
-              <td className="show-responsi992"> 02</td>
-              <td className="show-responsi992"> 02</td>
-              <td> VALOR $ 01 </td>
-              <td>
-                <div className="btn-red-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td className="show-responsi992"> #03</td>
-              <td className="data-show-responsi"> 08.08.08</td>
-              <td>Nome03</td>
-              <td className="show-responsi992"> 02</td>
-              <td className="show-responsi992"> 02</td>
-              <td> VALOR $ 01 </td>
-              <td>
-                <div className="btn-red-container">
-                  <NotificationButton />
-                </div>
-              </td>
-            </tr>
+            {sales.map((sale) => {
+              return (
+                <tr key={sale.id}>
+                  <td className="show-responsi992"> {sale.id}</td>
+                  <td className="data-show-responsi">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td>{sale.sellerName}</td>
+                  <td className="show-responsi992">{sale.visited}</td>
+                  <td className="show-responsi992"> {sale.deals}</td>
+                  <td>{sale.amount.toFixed(2)}</td>
+                  <td>
+                    <div className="btn-red-container">
+                      <NotificationButton />
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
